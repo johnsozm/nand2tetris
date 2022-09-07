db4ca3d46fb49dc8843dfce21d2369b2f9e7ad5b
johnsozm <z.m.johnson@protonmail.com>
2022-09-07 03:49:25 -0600

    Restructuring / renaming pass

--- src/main/java/nand2tetris/assembler/Assembler.java
+++ src/main/java/nand2tetris/assembler/Assembler.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.NumberFormat;
 
 /**
  * Primary assembler class. Assembles the .asm file specified on the command line into a .hack file.
@@ -25,8 +24,8 @@ public class Assembler {
         }
 
         //Generate output filename based on input filename
-        String hackFilename = "";
-        File hackFile = null;
+        String hackFilename;
+        File hackFile;
         if (!assemblyFilename.endsWith(".asm")) {
             System.out.println("Error: Assembler files should use the .asm extension.");
             return;
@@ -38,22 +37,21 @@ public class Assembler {
 
         //Pass 1: Populate symbol table with jump labels
         SymbolTable symbols = new SymbolTable();
-        Parser parser = null;
+        Parser parser;
         int commandLocation = -1;
         try {
             parser = new Parser(assemblyFile);
-        }
-        catch (FileNotFoundException e) {} //Check for file existence is above
-
-        while (parser.hasMoreCommands()) {
-            parser.advance();
-            if (parser.commandType() == Parser.CommandType.L_COMMAND) {
-                symbols.addEntry(parser.symbol(), commandLocation + 1);
-            }
-            else {
-                commandLocation++;
+            while (parser.hasMoreCommands()) {
+                parser.advance();
+                if (parser.commandType() == Parser.CommandType.L_COMMAND) {
+                    symbols.addEntry(parser.symbol(), commandLocation + 1);
+                }
+                else {
+                    commandLocation++;
+                }
             }
         }
+        catch (FileNotFoundException ignored) {} //Check for file existence is above
 
         //Pass 2: Translate file
         try {
@@ -61,52 +59,50 @@ public class Assembler {
             int nextMemorySlot = 16;
             try {
                 parser = new Parser(assemblyFile);
-            }
-            catch (FileNotFoundException e) {} //Check for file existence is above
+                while (parser.hasMoreCommands()) {
+                    parser.advance();
+                    switch (parser.commandType()) {
+                        case A_COMMAND:
+                            int value;
 
-            while (parser.hasMoreCommands()) {
-                parser.advance();
-                switch (parser.commandType()) {
-                    case A_COMMAND:
-                        //Determine value to be loaded
-                        int value = 0;
-
-                        //Check for literal integer load
-                        try {
-                            value = Integer.parseInt(parser.symbol());
-                        }
-
-                        //Check against symbol table, add if needed
-                        catch (NumberFormatException e) {
-                            if (symbols.contains(parser.symbol())) {
-                                value = symbols.getAddress(parser.symbol());
+                            //Check for literal integer load
+                            try {
+                                value = Integer.parseInt(parser.symbol());
                             }
-                            else {
-                                symbols.addEntry(parser.symbol(), nextMemorySlot);
-                                value = nextMemorySlot;
-                                nextMemorySlot++;
+
+                            //Check against symbol table, add if needed
+                            catch (NumberFormatException e) {
+                                if (symbols.contains(parser.symbol())) {
+                                    value = symbols.getAddress(parser.symbol());
+                                }
+                                else {
+                                    symbols.addEntry(parser.symbol(), nextMemorySlot);
+                                    value = nextMemorySlot;
+                                    nextMemorySlot++;
+                                }
                             }
-                        }
 
-                        //Translate load value to binary and write load command
-                        String binaryString = Integer.toString(value, 2);
-                        binaryString = "0".repeat(15 - binaryString.length()) + binaryString;
-                        writer.write("0" + binaryString + "\n");
-                        break;
+                            //Translate load value to binary and write load command
+                            String binaryString = Integer.toString(value, 2);
+                            binaryString = "0".repeat(15 - binaryString.length()) + binaryString;
+                            writer.write("0" + binaryString + "\n");
+                            break;
 
-                    case C_COMMAND:
-                        String comp = Code.comp(parser.comp());
-                        String dest = Code.dest(parser.dest());
-                        String jump = Code.jump(parser.jump());
-                        writer.write("111" + comp + dest + jump + "\n");
-                        break;
+                        case C_COMMAND:
+                            String comp = Code.comp(parser.comp());
+                            String dest = Code.dest(parser.dest());
+                            String jump = Code.jump(parser.jump());
+                            writer.write("111" + comp + dest + jump + "\n");
+                            break;
 
-                    case L_COMMAND:
-                        break;
+                        case L_COMMAND:
+                            break;
+                    }
                 }
-            }
 
-            writer.close();
+                writer.close();
+            }
+            catch (FileNotFoundException ignored) {} //Check for file existence is above
         }
         catch (IOException e) {
             System.out.println("IOException while trying to write .hack file.");
