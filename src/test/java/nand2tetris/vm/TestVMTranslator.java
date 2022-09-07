f7fbb9e49082dcbf96a80f948439f5f89271f635
johnsozm <z.m.johnson@protonmail.com>
2022-09-07 12:04:15 -0600

    Added new test files and test code to translate them

--- src/test/java/nand2tetris/vm/TestVMTranslator.java
+++ src/test/java/nand2tetris/vm/TestVMTranslator.java
@@ -7,32 +7,20 @@ import java.io.IOException;
 
 public class TestVMTranslator {
     @Test
-    public void testBasicTest() throws IOException {
-        VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/BasicTest.vm"});
-        assertTrue("Assembly code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/vm/BasicTest.asm"), new File("src/test/java/nand2tetris/vm/BasicTest.asm.cmp")));
+    public void testFibonacciElement() throws IOException {
+        VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/FibonacciElement"});
+        //TODO: Compare to known-good file
     }
 
     @Test
-    public void testPointerTest() throws IOException {
-        VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/PointerTest.vm"});
-        assertTrue("Assembly code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/vm/PointerTest.asm"), new File("src/test/java/nand2tetris/vm/PointerTest.asm.cmp")));
+    public void testNestedCall() throws IOException {
+        VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/NestedCall"});
+        //TODO: Compare to known-good file
     }
 
     @Test
-    public void testSimpleAdd() throws IOException {
-        VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/SimpleAdd.vm"});
-        assertTrue("Assembly code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/vm/SimpleAdd.asm"), new File("src/test/java/nand2tetris/vm/SimpleAdd.asm.cmp")));
-    }
-
-    @Test
-    public void testStackTest() throws IOException {
-        VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/StackTest.vm"});
-        assertTrue("Assembly code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/vm/StackTest.asm"), new File("src/test/java/nand2tetris/vm/StackTest.asm.cmp")));
-    }
-
-    @Test
-    public void testStaticTest() throws IOException {
-        VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/StaticTest.vm"});
-        assertTrue("Assembly code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/vm/StaticTest.asm"), new File("src/test/java/nand2tetris/vm/StaticTest.asm.cmp")));
+    public void testStaticsTest() throws IOException {
+        VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/StaticsTest"});
+        //TODO: Compare to known-good file
     }
 }
