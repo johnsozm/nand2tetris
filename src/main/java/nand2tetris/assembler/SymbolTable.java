5b505fdb1e5df1d250c18d052ab90c9b14932d6e
johnsozm <z.m.johnson@protonmail.com>
2022-09-07 02:44:57 -0600

    Implemented SymbolTable module

--- src/main/java/nand2tetris/assembler/SymbolTable.java
+++ src/main/java/nand2tetris/assembler/SymbolTable.java
@@ -1,15 +1,19 @@
 package nand2tetris.assembler;
 
+import java.util.HashMap;
+
 /**
  * Class which maintains the symbol table for an assembly file.
  */
 public class SymbolTable {
+    /**Storage for symbol mappings*/
+    private HashMap<String, Integer> map;
 
     /**
      * Creates a new empty symbol table.
      */
     public SymbolTable() {
-
+        map = new HashMap<String, Integer>();
     }
 
     /**
@@ -19,7 +23,7 @@ public class SymbolTable {
      * @param _address The address to assign to this symbol.
      */
     public void addEntry(String _symbol, int _address) {
-
+        map.put(_symbol, _address);
     }
 
     /**
@@ -29,16 +33,21 @@ public class SymbolTable {
      * @return True if the table contains this symbol, false if not.
      */
     public boolean contains(String _symbol) {
-        return false;
+        return map.containsKey(_symbol);
     }
 
     /**
      * Gets the address corresponding to the given symbol.
      *
      * @param _symbol The symbol to look up.
-     * @return The address assigned to this symbol/
+     * @return The address assigned to this symbol, or -1 if it is not in use.
      */
     public int getAddress(String _symbol) {
-       return 0;
+        if (map.containsKey(_symbol)) {
+            return map.get(_symbol);
+        }
+        else {
+            return -1;
+        }
     }
 }
