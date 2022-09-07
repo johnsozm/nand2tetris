f4b1ef93ac449d032aec415f56c3605c4d86ec84
johnsozm <z.m.johnson@protonmail.com>
2022-09-07 03:02:15 -0600

    Initial implementation of Parser class

--- src/main/java/nand2tetris/assembler/Parser.java
+++ src/main/java/nand2tetris/assembler/Parser.java
@@ -1,10 +1,28 @@
 package nand2tetris.assembler;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Class which parses .asm files into machine code
  */
 public class Parser {
+    /**Array of lines in the file*/
+    private ArrayList<String> lines;
+    /**Line currently being parsed*/
+    private int currentLine;
+    /**Command type of the last parsed line*/
+    private CommandType commandType;
+    /**Destination of the last parsed line*/
+    private String dest;
+    /**Computation of the last parsed line*/
+    private String comp;
+    /**Jump instruction of the last parsed line*/
+    private String jump;
+    /**Symbol used on the last parsed line*/
+    private String symbol;
+
     /**
      * Enum for command types
      */
@@ -22,15 +40,86 @@ public class Parser {
      *
      * @param _input The file to be read
      */
-    public Parser(File _input) {
+    public Parser(File _input) throws FileNotFoundException {
+        currentLine = -1;
+        lines = new ArrayList<>();
 
+        //Read in all lines, trimming excess whitespace and comments
+        Scanner scanner = new Scanner(_input);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            //Remove whitespace
+            line = line.replaceAll("[ \t]+", "");
+
+            //Remove comments if present
+            int trimIndex = line.indexOf("//");
+            if (trimIndex != -1) {
+                line = line.substring(0, trimIndex);
+            }
+
+            if (!line.equals("")) {
+                lines.add(line);
+            }
+        }
     }
 
     /**
-     * Advances parser to the next line of the assembly file.
+     * Checks if the assembly file has more commands remaining.
+     * @return True if there are more commands left, false if not.
+     */
+    public boolean hasMoreCommands() {
+        return currentLine < lines.size() - 1;
+    }
+
+    /**
+     * Advances parser to the next line of the assembly file and parses it.
      */
     public void advance() {
+        currentLine++;
+        String line = lines.get(currentLine);
 
+        //Determine command type and parse variables as needed
+        if (line.startsWith("@")) {
+            commandType = CommandType.A_COMMAND;
+            symbol = line.substring(1);
+            dest = "";
+            comp = "";
+            jump = "";
+        }
+        else if (line.startsWith("(")) {
+            commandType = CommandType.L_COMMAND;
+            symbol = line.substring(1, line.length() - 1);
+            dest = "";
+            comp = "";
+            jump = "";
+        }
+        else {
+            commandType = CommandType.C_COMMAND;
+            symbol = "";
+            int equalsIndex = line.indexOf("=");
+            int semicolonIndex = line.indexOf(";");
+
+            //Parse dest
+            if (equalsIndex == -1) {
+                dest = "";
+            }
+            else {
+                dest = line.substring(0, equalsIndex);
+            }
+
+            //Parse jump
+            if (semicolonIndex == -1) {
+                jump = "";
+                semicolonIndex = line.length();
+            }
+            else {
+                jump = line.substring(semicolonIndex + 1);
+            }
+
+            //Parse comp
+            comp = line.substring(equalsIndex + 1, semicolonIndex);
+        }
     }
 
     /**
@@ -39,7 +128,7 @@ public class Parser {
      * @return A CommandType enum corresponding to the current command's type.
      */
     public CommandType commandType() {
-        return null;
+        return commandType;
     }
 
     /**
@@ -49,7 +138,7 @@ public class Parser {
      * @return The symbol used on the current line.
      */
     public String symbol() {
-        return null;
+        return symbol;
     }
 
     /**
@@ -59,7 +148,7 @@ public class Parser {
      * @return The destination mnemonic of the current line.
      */
     public String dest() {
-        return null;
+        return dest;
     }
 
     /**
@@ -69,7 +158,7 @@ public class Parser {
      * @return The comparison mnemonic of the current line.
      */
     public String comp() {
-        return null;
+        return comp;
     }
 
     /**
@@ -79,6 +168,6 @@ public class Parser {
      * @return The jump mnemonic of the current line.
      */
     public String jump() {
-        return null;
+        return jump;
     }
 }
