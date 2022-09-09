6f2125920dd46768049aed3abc29a34ae929b116
johnsozm <z.m.johnson@protonmail.com>
2022-09-09 06:13:32 -0600

    Added known-good comparison files and updated test cases

--- src/test/java/nand2tetris/vm/TestVMTranslator.java
+++ src/test/java/nand2tetris/vm/TestVMTranslator.java
@@ -9,18 +9,20 @@ public class TestVMTranslator {
     @Test
     public void testFibonacciElement() throws IOException {
         VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/FibonacciElement"});
-        //TODO: Compare to known-good file
+        assertTrue("Assembler code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/vm/FibonacciElement/FibonacciElement.asm"), new File("src/test/java/nand2tetris/vm/FibonacciElement/FibonacciElement.asm.cmp")));
     }
 
     @Test
     public void testNestedCall() throws IOException {
         VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/NestedCall"});
-        //TODO: Compare to known-good file
+        assertTrue("Assembler code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/vm/NestedCall/NestedCall.asm"), new File("src/test/java/nand2tetris/vm/NestedCall/NestedCall.asm.cmp")));
+
     }
 
     @Test
     public void testStaticsTest() throws IOException {
         VMTranslator.main(new String[]{"src/test/java/nand2tetris/vm/StaticsTest"});
-        //TODO: Compare to known-good file
+        assertTrue("Assembler code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/vm/StaticsTest/StaticsTest.asm"), new File("src/test/java/nand2tetris/vm/StaticsTest/StaticsTest.asm.cmp")));
+
     }
 }
