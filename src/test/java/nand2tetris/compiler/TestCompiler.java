5eec4789f8d4426eb7604da06a6701ff1449f03d
johnsozm <z.m.johnson@protonmail.com>
2022-09-09 17:21:48 -0600

    Added specific test cases for first stage of compiler

--- src/test/java/nand2tetris/compiler/TestCompiler.java
+++ src/test/java/nand2tetris/compiler/TestCompiler.java
@@ -1,14 +1,23 @@
 package nand2tetris.compiler;
-
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
-
+import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 
 
 public class TestCompiler {
     @Test
-    public void compileAll() throws IOException {
-        JackCompiler.main(new String[]{"src/test/java/nand2tetris/compiler/"});
+    public void testArrayTest() throws IOException {
+        JackCompiler.main(new String[]{"src/test/java/nand2tetris/compiler/ArrayTest"});
+        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/ArrayTest/Main.vm"), new File("src/test/java/nand2tetris/compiler/ArrayTest/MainT.xml")));
+    }
+
+    @Test
+    public void testSquare() throws IOException {
+        JackCompiler.main(new String[]{"src/test/java/nand2tetris/compiler/Square"});
+        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/Main.vm"), new File("src/test/java/nand2tetris/compiler/Square/MainT.xml")));
+        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/Square.vm"), new File("src/test/java/nand2tetris/compiler/Square/SquareT.xml")));
+        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/SquareGame.vm"), new File("src/test/java/nand2tetris/compiler/Square/SquareGameT.xml")));
     }
 }
