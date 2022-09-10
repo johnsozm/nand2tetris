1b88bf2eef5cd19e983e258f6ba275a58a12c8c8
johnsozm <z.m.johnson@protonmail.com>
2022-09-09 18:08:39 -0600

    Updated test code to point to new comparison files.

--- src/test/java/nand2tetris/compiler/TestCompiler.java
+++ src/test/java/nand2tetris/compiler/TestCompiler.java
@@ -10,14 +10,14 @@ public class TestCompiler {
     @Test
     public void testArrayTest() throws IOException {
         JackCompiler.main(new String[]{"src/test/java/nand2tetris/compiler/ArrayTest"});
-        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/ArrayTest/Main.vm"), new File("src/test/java/nand2tetris/compiler/ArrayTest/MainT.cmp")));
+        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/ArrayTest/Main.vm"), new File("src/test/java/nand2tetris/compiler/ArrayTest/Main.cmp")));
     }
 
     @Test
     public void testSquare() throws IOException {
         JackCompiler.main(new String[]{"src/test/java/nand2tetris/compiler/Square"});
-        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/Main.vm"), new File("src/test/java/nand2tetris/compiler/Square/MainT.cmp")));
-        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/Square.vm"), new File("src/test/java/nand2tetris/compiler/Square/SquareT.cmp")));
-        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/SquareGame.vm"), new File("src/test/java/nand2tetris/compiler/Square/SquareGameT.cmp")));
+        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/Main.vm"), new File("src/test/java/nand2tetris/compiler/Square/Main.cmp")));
+        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/Square.vm"), new File("src/test/java/nand2tetris/compiler/Square/Square.cmp")));
+        assertTrue("VM code does not match", FileUtils.contentEquals(new File("src/test/java/nand2tetris/compiler/Square/SquareGame.vm"), new File("src/test/java/nand2tetris/compiler/Square/SquareGame.cmp")));
     }
 }
